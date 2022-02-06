3e83de33088d7a9c14934ba5ec786ac2140502d0
hisho <hisho.web@gmail.com>
2022-02-06 15:37:46 +0900

    ✨ change port & path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from '@src/app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('graphql')
   getHello(): string {
     return this.appService.getHello();
   }
